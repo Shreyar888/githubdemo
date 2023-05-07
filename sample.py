e17b27a90f26f195c79805b7524cb8c63cb21706
Shreyar888 <132807856+Shreyar888@users.noreply.github.com>
2023-05-07 16:02:58 +0530

    python test code 01

---
+++ sample.py
@@ -0,0 +1,9 @@
+myDict= {
+     "mandir" : "temple",
+     "sabji" : "vegetable",
+     "pankha"  : "fan",
+     "kapde" : "clothes"    
+ }
+ print("hindi words options are\n", myDict.keys())
+ a = input("write hindi word \n")
+ print("english translation of "+ a + " is", myDict[a])
